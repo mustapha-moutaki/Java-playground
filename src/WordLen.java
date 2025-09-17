e36a9dbb444dada1104b6459195660514997ebc8
mustapha <mustaphastar067@gmail.com>
2025-09-17 17:12:29 +0100

    start 4 ex

---
+++ src/WordLen.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordLen {
+    public static void main(String[] args) {
+
+    }
+
+    public static void CharCounter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the word: ");
+        String word = sc.nextLine();
+        sc.close();
+        int len = word.length();
+        ArrayList<Integer>Counter = new ArrayList<>();
+
+        if(len > 3){
+            for (int i = 0; i < len; i++) {
+                if(word)
+            }
+        }
+
+
+    }
+
+}
